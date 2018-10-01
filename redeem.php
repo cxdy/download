15388a766cde22609bb7b31e0f04dbabd20c1a21
Kyle Mahaney <contact@kylemahaney.com>
2018-10-01 15:57:54 -0400

    Update redeem.php

--- redeem.php
+++ redeem.php
@@ -11,7 +11,7 @@ include_once 'utilities.php';
   <form action="parseRedeem.php" method="POST">
     <input type="text" name="code" value="Redemption Code"></input>
     <input type="hidden" name="ip" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"></input>
-    <input type="hidden" name="token" value="<?php if(function_exists('_token')) echo _token(); ?>">
+    <input type="hidden" name="token" value="<?php if(function_exists('_token')) echo _token(); ?>"></input>
     <input type="submit" name="redeemBtn" value="Redeem"></input>
   </form>
 </body>
