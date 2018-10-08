30941828866fd57acd6ebfde7b0cbd52de4bc6b1
Cody Kaczynski <codylkaczynski@gmail.com>
2018-10-08 03:12:03 -0400

    Fixes #6

--- download.php
+++ download.php
@@ -12,34 +12,80 @@ include_once 'utilities.php';
     $jeff->execute();
     $result = $jeff->fetch(PDO::FETCH_ASSOC);
 
+    $ip = $_SERVER['REMOTE_ADDR'];
+
     $link = $result['link'];
     $downloads = $result['downloads'];
 
     if($link == $code) {
       if($downloads < 3) {
-        $file_name = "file.data";
-        header("Content-Disposition: attachment; filename=\"$file_name\"");
-        echo "Your download will start momentarily.";
-
-        $select->prepare('SELECT downloads FROM codes WHERE link = :code');
+        try{
+        $select= $db->prepare('SELECT downloads FROM codes WHERE link = :code');
         $select->bindParam(':code', $code, PDO::PARAM_STR);
         $select->execute();
         $bunghole = $select->fetch(PDO::FETCH_ASSOC);
 
-        $downloadCount = $bunghole + 1;
+        $downloadCount = $downloads + 1;
 
         $update = $db->prepare('UPDATE codes SET downloads = :downloadcount WHERE code = :code');
         $update->bindParam(':code', $code, PDO::PARAM_STR);
         $update->bindParam(':downloadcount', $downloadCount, PDO::PARAM_INT);
-        $update-execute();
+        $update->execute();
+      } catch (PDOException $ex){
+             $result = flashMessage("An error occurred: " .$ex->getMessage());
+        }
+
+        // Logging
+        date_default_timezone_set('America/New_York');
+        $timestamp = date('Y-m-d H:i:s');
+        $event = "Download Initiated";
+
+        try{
+            $logQuery = "INSERT INTO events (ip, code, timestamp, event)
+          VALUES (:ip, :code, :timestamp, :event)";
+            $statement = $db->prepare($logQuery);
+            $statement->execute(array(':ip' => $ip, ':code' => $code, ':timestamp' => $timestamp, ':event' => $event));
+          } catch (PDOException $ex){
+               $result = flashMessage("An error occurred: " .$ex->getMessage());
+          }
+
+        $file_name = "file.data";
+        header("Content-Disposition: attachment; filename=\"$file_name\"");
+        echo "Your download will start momentarily.";
 
         echo $downloadCount;
 
       } else {
-        echo "Fuck you";
+        echo "You have used your 3 downloads, please contact support if you feel this is incorrect.";
+        // Logging
+        date_default_timezone_set('America/New_York');
+        $timestamp = date('Y-m-d H:i:s');
+        $event = "Download limit reached";
+
+        try{
+            $logQuery = "INSERT INTO events (ip, code, timestamp, event)
+          VALUES (:ip, :code, :timestamp, :event)";
+            $statement = $db->prepare($logQuery);
+            $statement->execute(array(':ip' => $ip, ':code' => $code, ':timestamp' => $timestamp, ':event' => $event));
+          } catch (PDOException $ex){
+               $result = flashMessage("An error occurred: " .$ex->getMessage());
+          }
       }
     } else {
-      echo "You have used your 3 downloads. Contact support if you feel this is incorrect.";
+      echo "You have entered an incorrect download code.";
+      // Logging
+      date_default_timezone_set('America/New_York');
+      $timestamp = date('Y-m-d H:i:s');
+      $event = "Incorrect download code";
+
+      try{
+          $logQuery = "INSERT INTO events (ip, code, timestamp, event)
+        VALUES (:ip, :code, :timestamp, :event)";
+          $statement = $db->prepare($logQuery);
+          $statement->execute(array(':ip' => $ip, ':code' => $code, ':timestamp' => $timestamp, ':event' => $event));
+        } catch (PDOException $ex){
+             $result = flashMessage("An error occurred: " .$ex->getMessage());
+        }
     }
   }
 ?>
